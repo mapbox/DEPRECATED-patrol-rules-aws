c940bca5131b5d96a1bb298b773bfc556b0f0ce7
Ian Ward <ianshward@gmail.com>
2016-02-05 17:17:44 -0500

    bit more straight forward this way

---
+++ build.js
@@ -0,0 +1,12 @@
+var lambdaCfn = require('./lib/lambda-cfn');
+
+var rules = require('./index').rules;
+var built = [];
+
+rules.forEach(function(rule) {
+  built.push(lambdaCfn.build(rule.config));
+});
+
+var template = lambdaCfn.compile(built);
+
+console.log(template);
