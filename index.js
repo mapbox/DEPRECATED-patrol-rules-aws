3da2ecec42ea334cfcdfb0127b673c3e4cf05d98
Ian Ward <ianshward@gmail.com>
2016-02-07 23:25:40 -0500

    autoload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var path = require('path');
+var fs = require('fs');
 var streambot = require('streambot');
 
-module.exports.rules = [
-  require('./rules/myFunction'),
-  require('./rules/otherFunction')
-];
+var rulesPath = path.join(__dirname, 'rules');
 
-module.exports.rules.forEach(function(rule) {
-  module.exports[rule.config.name] = streambot(rule.fn);
+module.exports.rules = [];
+
+fs.readdirSync(rulesPath).forEach(function(file) {
+  if (path.extname(file) == '.js') {
+    var rule = require('./rules/' + file);
+    module.exports.rules.push(rule);
+    module.exports[rule.config.name] = streambot(rule.fn);
+  }
 });
