a7da6a954ba3f8ff3d438ccd1afeffee89eeaf08
Ian Ward <ianshward@gmail.com>
2016-02-10 23:47:22 -0500

    blacklisted roles

---
+++ rules/assumeRole.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: 'assumeRole',
+  parameters: {
+    'blacklistedRoles': {
+      'Type': 'String',
+      'Description': 'Comma separated list of blacklisted roles',
+    }
+  }
+};
+
+module.exports.fn = function(event, callback) {
+
+  var blacklisted = process.env.blacklistedRoles.split(/\s*,\s*/);
+  var assumedRoleArn = event.detail.requestParameters.roleArn;
+  var userName = detail.userIdentity.userName;
+  // Check for fuzzy match
+  blacklisted.forEach(function(role) {
+    if (assumedRoleArn.indexOf(role) > -1) {
+      return callback(err, 'Blacklisted role ' + role + ' assumed by ' + userName);
+    }
+  });
+  callback(err, 'Blacklisted role was not assumed');
+};
