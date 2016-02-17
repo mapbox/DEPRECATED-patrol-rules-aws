5ee8cd3f6929e0bcca839de35afc4fe3cfd9a628
Ian Ward <ianshward@gmail.com>
2016-02-17 00:03:50 -0500

    more tests

--- test/rules/blacklisted_resources.test.js
+++ test/rules/blacklisted_resources.test.js
@@ -117,7 +117,76 @@ test('blacklisted_resources rule', function(t) {
     t.equal(message.length, 1, 'There is only one result');
     t.equal(message[0].subject,
       'Policy allows access to blacklisted resources: arn:aws:s3:::foo/bar/baz, arn:aws:s3:::foo/bar',
-      'No matched blacklisted resources');
+      'Matches fuzzy match S3 blacklisted resources');
+  });
+
+  var docKinesisMatch = {
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "cloudtrail:*"
+        ]
+      },
+      {
+        Effect: "Allow",
+        Action: [
+          "kinesis:*"
+        ],
+        Resource: [
+          "arn:aws:kinesis:us-east-1:123456789012:stream/*-bar-*"
+        ]
+      }
+    ]
+  };
+
+  event.detail.requestParameters.policyDocument = JSON.stringify(docKinesisMatch);
+  process.env.blacklistedResources = 'arn:aws:kinesis:us-east-1:123456789012:stream/foo-bar-KinesisStream-ABC*, arn:aws:s3:::foo/bar';
+
+  fn(event, function(err, message) {
+    t.equal(message.length, 1, 'There is only one result');
+    t.equal(message[0].subject,
+      'Policy allows access to blacklisted resources: arn:aws:kinesis:us-east-1:123456789012:stream/foo-bar-KinesisStream-ABC*',
+      'Matches kinesis blacklisted resources');
+  });
+
+  var docTwoMatches = {
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "cloudtrail:*"
+        ]
+      },
+      {
+        Effect: "Allow",
+        Action: [
+          "kinesis:*"
+        ],
+        Resource: [
+          "arn:aws:kinesis:us-east-1:123456789012:stream/foo*"
+        ]
+      },
+      {
+        Effect: "Allow",
+        Action: [
+          "s3:*"
+        ],
+        Resource: [
+          "*"
+        ]
+      }
+    ]
+  };
+
+  event.detail.requestParameters.policyDocument = JSON.stringify(docTwoMatches);
+  process.env.blacklistedResources = 'arn:aws:kinesis:us-east-1:123456789012:stream/foo-bar-KinesisStream-ABC*, arn:aws:s3:::foo/bar';
+
+  fn(event, function(err, message) {
+    t.equal(message.length, 1, 'There is only one result');
+    t.equal(message[0].subject,
+      'Policy allows access to blacklisted resources: arn:aws:kinesis:us-east-1:123456789012:stream/foo-bar-KinesisStream-ABC*, arn:aws:s3:::foo/bar',
+      'Matches kinesis and s3 blacklisted resources');
   });
 
   t.end();
