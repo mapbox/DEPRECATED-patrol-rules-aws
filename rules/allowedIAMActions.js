a7e4182f6f1134afb7fe7abf07762fa077077895
Zack Mully <zack@925labs.com>
2016-06-07 18:39:39 -0400

    lambda-cfn v0.1.4

--- rules/allowedIAMActions.js
+++ rules/allowedIAMActions.js
@@ -1,6 +1,8 @@
 var message = require('lambda-cfn').message;
 var splitOnComma = require('lambda-cfn').splitOnComma;
 var getEnv = require('lambda-cfn').getEnv;
+var d3 = require('d3-queue');
+var policyProcessor = require('../lib/policyProcessor');
 
 module.exports.config = {
   name: 'allowedIAMActions',
@@ -33,48 +35,82 @@ module.exports.config = {
         ]
       }
     }
-  }
+  },
+  scheduledRule: 'rate(60 minutes)'
 };
 
 module.exports.fn = function(event, callback) {
-  if (event.detail.errorCode)
-    return callback(null, event.detail.errorMessage);
-
+  var batchedPolicies;
+  var q = d3.queue();
   var allowedActions = splitOnComma(getEnv('allowedActions'));
-  var document = JSON.parse(event.detail.requestParameters.policyDocument);
   var restrictedServices = splitOnComma(getEnv('restrictedServices'));
 
-  // build list of actions used.
-  var actions = [];
-  document.Statement.forEach(function(policy) {
-    if (!Array.isArray(policy.Action))
-      actions.push(policy.Action);
-    else
-      actions = actions.concat(policy.Action);
-  });
-
-  var violations = [];
-  actions.forEach(function(pair) {
-    var parts = pair.split(':');
-    var service = parts[0];
-
-    // Check if a restricted service, and not on the allowed list.
-    if (restrictedServices.indexOf(service) > -1 && allowedActions.indexOf(pair) < 0) {
-      violations.push(pair);
-    }
-  });
-
-  if (violations.length > 0) {
-    var notif = {
-      subject: 'Disallowed actions used in policy',
-      summary: 'Disallowed actions ' + violations.join(' ') + ' used in policy',
-      event: event
-    };
-    message(notif, function(err, result) {
-      callback(err, result);
+  if (event['detail-type'] == 'Scheduled Event') {
+    //call policy processor
+    policyProcessor(function(err,data) {
+      batchedPolicies = data;
+      //console.log("BATCHED POLICIES: " + JSON.stringify(batchedPolicies));
+      for (var role in batchedPolicies) {
+        for (var policy in batchedPolicies[role].policies) {
+          var policyDetail = {
+            roleName: role,
+            policyName: policy,
+            policyDocument: batchedPolicies[role].policies[policy]
+          };
+         q.defer(processPolicy, policyDetail);
+        }
+      }
+      q.awaitAll(function(err, data) {
+        return callback(err,data);
+      });
     });
+  } else if (event.detail.errorCode) {
+    return callback(null, event.detail.errorMessage);
   } else {
-    callback(null, 'Disallowed action was not used in policy');
+    processPolicy(event.detail.requestParameters, function(err,data) {
+      if (err) return callback(err);
+      else return callback(null,data);
+    });
   }
 
+  function processPolicy(policyDetail, next) {
+    var document = JSON.parse(policyDetail.policyDocument);
+    // build list of actions used.
+    var actions = [];
+    document.Statement.forEach(function(policy) {
+      if (!Array.isArray(policy.Action))
+        actions.push(policy.Action);
+      else
+        actions = actions.concat(policy.Action);
+    });
+
+    var violations = [];
+    actions.forEach(function(pair) {
+      var parts = pair.split(':');
+      var service = parts[0];
+
+      // Check if a restricted service, and not on the allowed list.
+      if (restrictedServices.indexOf(service) > -1 && allowedActions.indexOf(pair) < 0) {
+        violations.push(pair);
+      }
+    });
+
+    if (violations.length > 0) {
+      var notif = {
+        subject: 'Disallowed actions used in policy',
+        summary: 'Disallowed actions ' + violations.join(' ') + ' used in policy'
+      };
+      if (batchedPolicies) {
+        notif.event = policyDetail;
+      } else {
+        notif.event = event;
+      }
+
+      message(notif, function(err, result) {
+        next(err, result);
+      });
+    } else {
+      next(null, 'Disallowed action was not used in policy');
+    }
+  };
 };
--- rules/disallowedResources.js
+++ rules/disallowedResources.js
@@ -3,6 +3,8 @@ var d3 = require('d3-queue');
 var message = require('lambda-cfn').message;
 var splitOnComma = require('lambda-cfn').splitOnComma;
 var getEnv = require('lambda-cfn').getEnv;
+var policyProcessor = require('../lib/policyProcessor');
+var util =  require('util');
 
 module.exports.config = {
   name: 'disallowedResources',
@@ -31,84 +33,136 @@ module.exports.config = {
         ]
       }
     }
-  }
+  },
+  scheduledRule: 'rate(120 minutes)'
 };
 
 module.exports.fn = function(event, callback) {
-  if (event.detail.errorCode)
-    return callback(null, event.detail.errorMessage);
   var iam = new AWS.IAM();
-  var q = d3.queue(1);
-
+  var q = d3.queue();
+  var batchedPolicies;
   var disallowedResources = splitOnComma(getEnv('disallowedResourceArns'));
-  var document = event.detail.requestParameters.policyDocument;
-  var parsed = JSON.parse(document);
 
-  var simulate = function(params, cb) {
-    iam.simulateCustomPolicy(params, function(err, data) {
-      cb(err, data);
-    });
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
   };
 
-  disallowedResources.forEach(function(resource) {
-    var resourceService = resource.split(':')[2];
-    parsed.Statement.forEach(function(policy) {
-      var actions = [];
-      if (policy.Effect == 'Allow' && policy.Action) {
-        actions = typeof policy.Action == 'string' ? [policy.Action] : policy.Action;
+  //var document = event.detail.requestParameters.policyDocument;
+  //var parsed = JSON.parse(document);
+  //console.log(event);
+  if (event['detail-type'] == 'Scheduled Event') {
+    //call policy processor
+    policyProcessor(function(err,data) {
+      batchedPolicies = data;
+      //console.log(JSON.stringify(batchedPolicies));
+      for (var role in batchedPolicies) {
+        for (var policy in batchedPolicies[role].policies) {
+          var policyDetail = {
+            roleName: role,
+            policyName: policy,
+            policyDocument: batchedPolicies[role].policies[policy]
+          };
+          if (policyDetail.policyDocument) {
+            q.defer(processPolicy, policyDetail);
+          } else {
+            console.log('ERROR:%s:%s',policyDetail.roleName, policyDetail.policyName);
+          }
+        }
+      }
+      q.awaitAll(function(err, data) {
+         return callback(err,data);
+      });
+    });
+  } else if (event.detail.errorCode) {
+    return callback(null, event.detail.errorMessage);
+  } else {
+    processPolicy(event.detail.requestParameters, function(err,data) {
+      if (err) return callback(err);
+      else return callback(null,data);
+    });
+  }
+
+  function processPolicy(policyDetail, next) {
+    var q = d3.queue(1);
+    var simulate = function(params, cb) {
+      iam.simulateCustomPolicy(params, function(err, data) {
+        cb(err, data);
+      });
+    };
+    // try {
+       var parsed = JSON.parse(policyDetail.policyDocument);
+    // } catch(e) {
+    //   console.log(util.format('Error %s parsing %s',e,policyDetail.policyDocument));
+    // }
+
+    disallowedResources.forEach(function(resource) {
+      var resourceService = resource.split(':')[2];
+      parsed.Statement.forEach(function(policy) {
+        //console.log(policy);
+        var actions = [];
+        if (policy.Effect == 'Allow' && policy.Action) {
+          actions = typeof policy.Action == 'string' ? [policy.Action] : policy.Action;
+        }
+        //console.log(actions);
+        actions.forEach(function(action) {
+          var policyService = action.split(':')[0];
+          if (resourceService === policyService) {
+            // A disallowed resource's service matches one of the services in
+            // a policy.  Run through simulator.
+            var params = {
+              ActionNames: [action],
+              PolicyInputList: [policyDetail.policyDocument],
+              ResourceArns: [resource]
+            };
+            q.defer(simulate, params);
+          }
+        });
+      });
+    });
+
+    q.awaitAll(function(err, data) {
+      if (err) return callback(err);
+      var matches = [];
+      var truncated = false;
+      data.forEach(function(response) {
+        // Warn on truncation.  Build paging support if this is hit.
+        if (response.IsTruncated) truncated = true;
+        response.EvaluationResults.forEach(function(result) {
+          if (result.EvalDecision === 'allowed') {
+            matches.push(result.EvalResourceName);
+          }
+        });
+      });
+
+      // Report
+      var q = d3.queue(1);
+      if (truncated) {
+        q.defer(message, {
+          subject: 'Disallowed resources rule results truncated',
+          summary: 'Disallowed resources rule results were truncated. Paging ' +
+            'is not currently supported.'
+        });
       }
 
-      actions.forEach(function(action) {
-        var policyService = action.split(':')[0];
-        if (resourceService === policyService) {
-          // A disallowed resource's service matches one of the services in
-          // a policy.  Run through simulator.
-          var params = {
-            ActionNames: [action],
-            PolicyInputList: [document],
-            ResourceArns: [resource]
-          };
-          q.defer(simulate, params);
+      if (matches.length) {
+
+        var summaryMatches = matches.filter(onlyUnique);
+
+        var notif = {
+          subject: 'Policy allows access to disallowed resources',
+          summary: 'Policy allows access to disallowed resources: ' + summaryMatches.join(', ')
+        };
+        if (batchedPolicies) {
+          notif.event = policyDetail;
+        } else {
+          notif.event = event;
         }
+        q.defer(message, notif);
+      }
+
+      q.awaitAll(function(err, ret) {
+        callback(err, ret);
       });
     });
-  });
-
-  q.awaitAll(function(err, data) {
-    if (err) return callback(err);
-    var matches = [];
-    var truncated = false;
-    data.forEach(function(response) {
-      // Warn on truncation.  Build paging support if this is hit.
-      if (response.IsTruncated) truncated = true;
-      response.EvaluationResults.forEach(function(result) {
-        if (result.EvalDecision === 'allowed') {
-          matches.push(result.EvalResourceName);
-        }
-      });
-    });
-
-    // Report
-    var q = d3.queue(1);
-    if (truncated) {
-      q.defer(message, {
-        subject: 'Disallowed resources rule results truncated',
-        summary: 'Disallowed resources rule results were truncated. Paging ' +
-          'is not currently supported.'
-      });
-    }
-
-    if (matches.length) {
-      q.defer(message, {
-        subject: 'Policy allows access to disallowed resources',
-        summary: 'Policy allows access to disallowed resources: ' + matches.join(', '),
-        event: event
-      });
-    }
-
-    q.awaitAll(function(err, ret) {
-      callback(err, ret);
-    });
-  });
-
+  };
 };
