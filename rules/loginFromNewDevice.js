3f0f4519cc8977cfb782dd447438e267c3b451e7
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-06-07 23:47:58 -0400

    Basic layout of rule for unknown devices

---
+++ rules/loginFromNewDevice.js
@@ -0,0 +1,69 @@
+var message = require('lambda-cfn').message;
+
+module.exports.config = {
+  name: 'loginFromNewDevice',
+  sourcePath: 'rules/loginFromNewDevice.js',
+  parameters: {},
+  eventRule: {
+    eventPattern: {
+      'detail-type': [
+        'AWS API Call via CloudTrail'
+      ],
+      detail: {
+        eventSource: [
+          'signin.amazonaws.com'
+        ],
+        eventName: [
+          'ConsoleLogin'
+        ]
+      }
+    }
+  }
+};
+
+module.exports.fn = function(evt, cb) {
+  var hash = generateDeviceIdentity(evt);
+  var note = generateNotification(evt);
+
+  if (isNewDevice(hash)) message(note, function(err, res) { cb(err, res) });
+  else cb(null, ':ok_hand:');
+};
+
+/**
+ * Generate a hash to identify a user's device
+ * TODO: implement the real hashing thing
+ * @param {object} evt is a CloudTrail event
+ * @return {string} hash the identifies a user's device
+ */
+function generateDeviceIdentity(evt) {
+  return evt.userIdentity.arn + evt.userAgent;
+};
+
+/**
+ * Generate a notification object to be sent to patrol
+ * @param {object} evt is a CloudTrail event
+ * @return {object} following the patrol spec
+ */
+function generateNotification(evt) {
+  var u = evt.userIdentity.principalId;
+  var d = evt.userAgent;
+  return {
+    subject: 'Login from a new device',
+    summary: 'First time ' + u + ' logins from ' + d,
+    event: evt
+  }
+};
+
+/**
+ * Match a user's device hash against a list of known devices
+ * TODO: use an external storage, e.g. S3
+ * @param {string} dhash is a hash that identifies a user's device
+ * @return {boolean} to determine if a user's device is new
+ */
+function isNewDevice(dhash) {
+  return dhash.match(/Safari/);
+};
+
+module.exports.generateDeviceIdentity = generateDeviceIdentity;
+module.exports.generateNotification = generateNotification;
+module.exports.isNewDevice = isNewDevice;
---
+++ test/rules/loginFromNewDevice.test.js
@@ -0,0 +1,55 @@
+var test = require('tape');
+
+var rule = require('../../rules/loginFromNewDevice');
+var fn = rule.fn;
+var name = rule.config.name;
+
+var evt = {
+	"eventVersion": "1.02",
+	"userIdentity": {
+		"type": "AssumedRole",
+		"principalId": "ABCDEFGHIJKL:lolcat",
+		"arn": "arn:aws:sts::13371337:assumed-role/evilcorp-iam-groups-roles-production-HackerRole-1LEFOO1001/lolcat",
+		"accountId": "13371337",
+		"sessionContext": {
+			"attributes": {
+				"mfaAuthenticated": "true",
+				"creationDate": "2016-06-08T00:28:05Z"
+			},
+			"sessionIssuer": {
+				"type": "Role",
+				"principalId": "ABCDEFGHIJKL",
+				"arn": "arn:aws:iam::13371337:role/evilcorp-iam-groups-roles-production-HackerRole-1LEFOO1001",
+				"accountId": "13371337",
+				"userName": "evilcorp-iam-groups-roles-production-HackerRole-1LEFOO1001"
+			}
+		}
+	},
+	"eventTime": "2016-06-08T00:28:07Z",
+	"eventSource": "signin.amazonaws.com",
+	"eventName": "ConsoleLogin",
+	"awsRegion": "us-east-1",
+	"sourceIPAddress": "50.1.124.10",
+	"userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36",
+	"requestParameters": null,
+	"responseElements": {
+		"ConsoleLogin": "Success"
+	},
+	"additionalEventData": {
+		"MobileVersion": "No",
+		"MFAUsed": "No"
+	},
+	"eventID": "foobar-15f5-417e-1010-0c63b33f44e6",
+	"eventType": "AwsConsoleSignIn",
+	"recipientAccountId": "13371337"
+};
+
+
+test('Login from new device', function (t) {
+  t.plan(2);
+
+  fn(evt, function (err, message) {
+    t.error(err, 'No error when calling ' + name);
+    t.equal(message, 'User login from a new device');
+  });
+});
