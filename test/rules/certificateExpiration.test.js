3d0ab9f0c0a681b55a4f524f960c6953a8479d0f
calfarome <cesar@mapbox.com>
2016-11-10 17:01:49 -0500

    updated indentation

--- test/rules/certificateExpiration.test.js
+++ test/rules/certificateExpiration.test.js
@@ -83,40 +83,36 @@ var list2 = {
 	}]
 }
 
-  var list3 = {
-    "ServerCertificateMetadataList": [
-        {
-            "Arn": "arn:aws:iam::6",
-            "UploadDate": "2016-01-11T21:24:08Z",
-            "ServerCertificateName": "routerw",
-            "ServerCertificateId": "ASssss4",
-            "Path": "/",
-            "Expiration": "2016-11-20T04:40:59Z"
-        },
-        {
-            "Arn": "asssqqqqeeerr",
-            "UploadDate": "2015-04-24T03:34:19Z",
-            "ServerCertificateName": "gddddd",
-            "ServerCertificateId": "ASCefaaa",
-            "Path": "/cloudfront/",
-            "Expiration": "2017-05-30T12:00:00Z"
-        },
-        {
-            "Arn": "arn:aws:iam::234858wwwwww",
-            "UploadDate": "2016-05-26T13:28:51Z",
-            "ServerCertificateName": "aaaaaaafgfdbgb",
-            "ServerCertificateId": "ASCAINCB5YRT2GYSLN4VC",
-            "Path": "/cloudfront/",
-            "Expiration": "2018-05-31T12:00:00Z"
-        },
-        {
-            "Arn": "assssqqqqqqq",
-            "UploadDate": "2015-04-24T03:34:19Z",
-            "ServerCertificateName": "gddddd",
-            "ServerCertificateId": "ASCefaaa",
-            "Path": "/cloudfront/",
-            "Expiration": "2017-04-27T12:00:00Z"
-        }
-        
-    ]
-} 
+var list3 = {
+	"ServerCertificateMetadataList": [{
+			"Arn": "arn:aws:iam::6",
+			"UploadDate": "2016-01-11T21:24:08Z",
+			"ServerCertificateName": "routerw",
+			"ServerCertificateId": "ASssss4",
+			"Path": "/",
+			"Expiration": "2016-11-20T04:40:59Z"
+		}, {
+			"Arn": "asssqqqqeeerr",
+			"UploadDate": "2015-04-24T03:34:19Z",
+			"ServerCertificateName": "gddddd",
+			"ServerCertificateId": "ASCefaaa",
+			"Path": "/cloudfront/",
+			"Expiration": "2017-05-30T12:00:00Z"
+		}, {
+			"Arn": "arn:aws:iam::234858wwwwww",
+			"UploadDate": "2016-05-26T13:28:51Z",
+			"ServerCertificateName": "aaaaaaafgfdbgb",
+			"ServerCertificateId": "ASCAINCB5YRT2GYSLN4VC",
+			"Path": "/cloudfront/",
+			"Expiration": "2018-05-31T12:00:00Z"
+		}, {
+			"Arn": "assssqqqqqqq",
+			"UploadDate": "2015-04-24T03:34:19Z",
+			"ServerCertificateName": "gddddd",
+			"ServerCertificateId": "ASCefaaa",
+			"Path": "/cloudfront/",
+			"Expiration": "2017-04-27T12:00:00Z"
+		}
+
+	]
+}
