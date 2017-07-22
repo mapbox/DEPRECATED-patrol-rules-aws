07605120be54514860f5f8684e752a322ee81472
Zachariah Mully <mully@mapbox.com>
2017-07-21 21:16:01 -0400

    initial work on refreshing service limits

--- rules/serviceLimits.js
+++ rules/serviceLimits.js
@@ -35,54 +35,84 @@ module.exports.fn = function(event, context, callback) {
 
   var support = new AWS.Support({region: 'us-east-1'});
 
-  support.describeTrustedAdvisorCheckResult(params, function(err, data) {
-    if (err) return callback(err); // an error occurred
-    var notIgnored = [];
-    data.result.flaggedResources.forEach(function(key) {
-      if (key.status != 'ok') {
-        if (ignored) {
-          ignored.filter(function(resource) {
-            if (!(key.resourceId.indexOf(resource) > -1)) {
-              notIgnored.push(key);
-            };
-          });
-        } else {
-          notIgnored.push(key);
+  function refreshCheck(callback) {
+    support.refreshTrustedAdvisorCheck(params, function(err, data) {
+      if (err) return callback(err, err.stack); // an error occurred
+      if (data) {
+        switch(data.status.status) {
+        case 'none':
+        case 'abandoned':
+          console.log('Refresh status none|abandoned, pausing for 15sec');
+          setTimeout(refreshCheck(callback), 15000);
+          break;
+        case 'enqueued':
+        case 'processing':
+          console.log('Refresh status state is processing, pausing for 15sec');
+          setTimeout(refreshCheck(callback), 15000);
+          break;
+        case 'success':
+          getCheckResult(callback);
+          break;
+        default:
+          return callback('ERROR: refresh check returned unknown status code');
+          break;
         }
       }
     });
+  }
 
-    if (notIgnored.length > 0) {
-      var warning = [];
-      notIgnored.forEach(function(k, i) {
-        warning[i] = util.format('Service: %s \nResource: %s \nRegion: %s \nLimit: %s \nCurrent: %s \nResourceId: %s\n\n', k.metadata[1], k.metadata[2], k.metadata[0], k.metadata[3], k.metadata[4], k.resourceId);
+  function getCheckResult(callback) {
+    support.describeTrustedAdvisorCheckResult(params, function(err, data) {
+      if (err) return callback(err); // an error occurred
+      var notIgnored = [];
+      data.result.flaggedResources.forEach(function(key) {
+        if (key.status != 'ok') {
+          if (ignored) {
+            ignored.filter(function(resource) {
+              if (!(key.resourceId.indexOf(resource) > -1)) {
+                notIgnored.push(key);
+              };
+            });
+          } else {
+            notIgnored.push(key);
+          }
+        }
       });
 
-      if (notIgnored.length == 1) {
-        if (notIgnored[0].metadata[0] == '-') {
-          var region = 'all regions';
+      if (notIgnored.length > 0) {
+        var warning = [];
+        notIgnored.forEach(function(k, i) {
+          warning[i] = util.format('Service: %s \nResource: %s \nRegion: %s \nLimit: %s \nCurrent: %s \nResourceId: %s\n\n', k.metadata[1], k.metadata[2], k.metadata[0], k.metadata[3], k.metadata[4], k.resourceId);
+        });
+
+        if (notIgnored.length == 1) {
+          if (notIgnored[0].metadata[0] == '-') {
+            var region = 'all regions';
+          } else {
+            var region = notIgnored[0].metadata[0];
+          }
+
+          var notif = {
+            subject: util.format('Service limit warning for %s in %s', notIgnored[0].metadata[1], region),
+            summary: warning.join(''),
+            event: notIgnored
+          };
         } else {
-          var region = notIgnored[0].metadata[0];
+          var notif = {
+            subject: 'Service limit warning for multiple services',
+            summary: warning.join(''),
+            event: notIgnored
+          };
         }
 
-        var notif = {
-          subject: util.format('Service limit warning for %s in %s', notIgnored[0].metadata[1], region),
-          summary: warning.join(''),
-          event: notIgnored
-        };
+        message(notif, function(err, result) {
+          callback(err, result);
+        });
       } else {
-        var notif = {
-          subject: 'Service limit warning for multiple services',
-          summary: warning.join(''),
-          event: notIgnored
-        };
+        callback(null, 'No service limit warning found');
       }
+    });
+  }
 
-      message(notif, function(err, result) {
-        callback(err, result);
-      });
-    } else {
-      callback(null, 'No service limit warning found');
-    }
-  });
+
 };
