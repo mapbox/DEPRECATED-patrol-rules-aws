6c113da399d31bca57ae900b18eed5d198be23c1
Alexandra Ulsh <ulsh@mapbox.com>
2018-02-15 19:41:30 -0500

    Adds rule to detect for disabling AWS managed S3 encryption

---
+++ test/removeS3ManagedEncryption.test.js
@@ -0,0 +1,26 @@
+const test = require('tape');
+
+const rule = require('../removeS3ManagedEncryption/function.js');
+const fn = rule.fn;
+
+test('Alerts if S3 managed encryption removed from bucket', (t) => {
+
+  let event = {
+    'detail': {
+      'eventSource': 's3.amazonaws.com',
+      'eventName': 'DeleteBucketEncryption',
+      'requestParameters': {
+        'encryption': [ '' ],
+        'bucketName': 'i-has-a-bucket'
+      }
+    }
+  };
+
+  fn(event, {}, (err, message) => {
+    console.log(err);
+    t.error(err, 'does not error');
+    t.equal(message.subject, 'Bucket encryption removed from i-has-a-bucket', 'Should alarm when encryption removed');
+    t.end();
+  });
+
+});
