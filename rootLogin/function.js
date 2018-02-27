bfc6caf1a86254d564ab8685031e86eb47337dda
Edgar Marca <matiskay@gmail.com>
2018-02-27 15:39:19 -0500

    Fix rootLogin patrol rule

--- rootLogin/function.js
+++ rootLogin/function.js
@@ -3,13 +3,14 @@ const message = require('@mapbox/lambda-cfn').message;
 module.exports.fn = function(event, context, callback) {
   if (event.detail.errorCode) return callback(null, event.detail.errorMessage);
 
-  if (event.detail.userIdentity.userName === 'root') {
+  if (event.detail.userIdentity.type === 'Root') {
     let notif = {
       subject: 'Root user logged into the console.',
       summary: 'Patrol detected that the root AWS user logged into the console',
       event: event
     };
     message(notif, (err, result) => {
+      console.log(JSON.stringify(notif));
       callback(err, result);
     });
   } else {
