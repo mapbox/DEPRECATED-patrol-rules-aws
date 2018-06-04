f9d0d6b1a00e4bfbcf0cb381870c8885645dedd7
Zachariah Mully <mully@mapbox.com>
2018-06-04 17:19:08 -0400

    typo

---
+++ principalPolicySimulator/function.js
@@ -0,0 +1,93 @@
+const AWS = require('aws-sdk');
+const d3 = require('d3-queue');
+const message = require('@mapbox/lambda-cfn').message;
+const splitOnComma = require('@mapbox/lambda-cfn').splitOnComma;
+
+module.exports.fn = (event, context, callback) => {
+  if (event.detail.errorCode) return callback(null, event.detail.errorMessage);
+  let iam = new AWS.IAM();
+  let q = d3.queue(1);
+  let principal;
+
+  try {
+    let arnRegex = new RegExp(process.env.principalRegex, 'i');
+  } catch (e) {
+    console.log(`ERROR: Invalid regex ${process.env.principalRegex}, ${e}`);
+    return callback(e);
+  }
+
+  if (arnRegex.test(event.userIdentity.sessionContext.sessionIssuer.arn)) {
+    principal = event.userIdentity.sessionContext.sessionIssuer.arn;
+  } else {
+    console.log(`INFO: skipping principal ${event.userIdentity.sessionContext.sessionIssuer.arn}`);
+    return callback();
+  }
+
+  let document = event.detail.requestParameters.policyDocument;
+  let parsed = JSON.parse(document);
+
+  let simulate = function(params, cb) {
+    iam.simulatePrincipalPolicy(params, (err, data) => {
+      cb(err, data);
+    });
+  };
+
+  parsed.Statement.forEach((policy) => {
+    policyProcessor(policy);
+  });
+
+  function policyProcessor(policy) {
+    let actions = [];
+    let resources = [];
+    if (policy.Effect === 'Allow' && policy.Action) {
+      actions = typeof policy.Action === 'string' ? [policy.Action] : policy.Action;
+    }
+    resources = typeof policy.Resource === 'string' ? [policy.Resource] : policy.Resource;
+
+    let params = {
+      PolicySourceArn: principal,
+      ActionNames: actions,
+      ResourceNames: resources
+    };
+    q.defer(simulate, params);
+  }
+
+  q.awaitAll(function(err, data) {
+    if (err) return callback(err);
+    let matches = [];
+    let truncated = false;
+    data.forEach(function(response) {
+      // Warn on truncation.  Build paging support if this is hit.
+      if (response.IsTruncated) truncated = true;
+      response.EvaluationResults.forEach((result) => {
+        if (result.EvalDecision === 'denied') {
+          matches.push(result.EvalResourceName);
+        }
+      });
+    });
+
+    // Report
+    let q = d3.queue(1);
+    if (truncated) {
+      q.defer(message, {
+        subject: 'Principal policy rule results truncated',
+        summary: 'Principal policy  rule results were truncated. Paging ' +
+          'is not currently supported.'
+      });
+    }
+
+    let iamResource = parsed.requestParameters.policyArn ? parsed.requestParameters.policy : parsed.requestParameters.roleName;
+
+    if (matches.length) {
+      q.defer(message, {
+        subject: `Principcal ${principal} allowed access to restricted resource via ${iamResource}`,
+        summary: `Principcal ${principal} allowed access to restricted resource via ${iamResource}:  ${matches.join(', ')}`,
+        event: event
+      });
+    }
+
+    q.awaitAll((err, ret) => {
+      callback(err, ret);
+    });
+  });
+};
---
+++ principalPolicySimulator/function.template.js
@@ -0,0 +1,32 @@
+var lambdaCfn = require('@mapbox/lambda-cfn');
+
+module.exports = lambdaCfn.build({
+  name: 'principalPolicySimulator',
+  parameters: {
+    principalRegex: {
+      Type: 'String',
+      Description: 'IAM Principal ARN matching regex, or "none" to test all principals'
+    }
+  },
+  eventSources: {
+    cloudwatchEvent: {
+      eventPattern:{
+        'detail-type': [
+          'AWS API Call via CloudTrail'
+        ],
+        detail: {
+          eventSource: [
+            'iam.amazonaws.com'
+          ],
+          eventName: [
+            'CreatePolicy',
+            'CreatePolicyVersion',
+            'PutGroupPolicy',
+            'PutRolePolicy',
+            'PutUserPolicy'
+          ]
+        }
+      }
+    }
+  }
+});
