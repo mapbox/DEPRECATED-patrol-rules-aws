8065f9d07a4695c65d35eb39bb4f1b26ed8c80c8
Kara Mahoney <kara.mahoney@mapbox.com>
2019-05-17 17:21:13 -0400

    add bucket filter to removeS3AccessLogging rule

--- removeS3AccessLogging/function.template.js
+++ removeS3AccessLogging/function.template.js
@@ -2,6 +2,12 @@ var lambdaCfn = require('@mapbox/lambda-cfn');
 
 module.exports = lambdaCfn.build({
   name: 'removeS3AccessLogging',
+  parameters: {
+    bucketFilter: {
+      Type: 'String',
+      Description: 'Comma separated list of buckets the rule will ignore'
+    }
+  },
   eventSources: {
     cloudwatchEvent: {
       eventPattern: {
