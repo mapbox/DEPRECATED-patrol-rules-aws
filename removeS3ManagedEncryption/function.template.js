0ebd24f32385f08b92549d757fb86b0fd90ac2ad
Kara Mahoney <kara.mahoney@mapbox.com>
2019-05-17 17:29:25 -0400

    add bucket filter to removeS3ManagedEncryption rule

--- removeS3ManagedEncryption/function.template.js
+++ removeS3ManagedEncryption/function.template.js
@@ -2,6 +2,12 @@ const lambdaCfn = require('@mapbox/lambda-cfn');
 
 module.exports = lambdaCfn.build({
   name: 'removeS3ManagedEncryption',
+  parameters: {
+    bucketFilter: {
+      Type: 'String',
+      Description: 'Comma separated list of bucket names or name patterns the rule will ignore'
+    }
+  },
   eventSources: {
     cloudwatchEvent: {
       eventPattern: {
--- test/removeS3ManagedEncryption.test.js
+++ test/removeS3ManagedEncryption.test.js
@@ -1,10 +1,8 @@
 const test = require('tape');
-
 const rule = require('../removeS3ManagedEncryption/function.js');
 const fn = rule.fn;
 
 test('Alerts if S3 managed encryption removed from bucket', (t) => {
-
   const event = {
     'detail': {
       'eventSource': 's3.amazonaws.com',
@@ -21,5 +19,24 @@ test('Alerts if S3 managed encryption removed from bucket', (t) => {
     t.equal(message.subject, 'Bucket encryption removed from i-has-a-bucket', 'Should alarm when encryption removed');
     t.end();
   });
+});
 
+test('Do not trigger notification for bucket listed in bucketFilter', (t) => {
+  process.env.bucketFilter = 'unencrypted';
+  const event = {
+    'detail': {
+      'eventSource': 's3.amazonaws.com',
+      'eventName': 'DeleteBucketEncryption',
+      'requestParameters': {
+        'encryption': [ '' ],
+        'bucketName': 'unencrypted-bucket'
+      }
+    }
+  };
+
+  fn(event, {}, (err, message) => {
+    t.error(err, 'does not error');
+    t.equal(message, 'S3 bucket unencrypted-bucket filtered, encryption change ignored', 'Should report that bucket was in filter');
+    t.end();
+  });
 });
